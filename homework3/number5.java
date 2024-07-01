8f1e30141b3701544267d86fcdeaffb401ec5fdf
Hiếu Nguyễn Trần Văn <hieukn1@gmail.com>
2024-07-01 11:39:46 +0700

    number10

--- homework3/number5.java
+++ homework3/number5.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 public class number5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("Nhập n");
+        int n = scanner.nextInt();
+        for (int i = 1; i <= 2*n; i+=2){
+            System.out.println(i);
+        }
     }
 }
